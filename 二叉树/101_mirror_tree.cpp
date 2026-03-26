17a518c2b57b4f6226f9d402e6a077c86db4232b
DCdengchao <1149327963@qq.com>
2026-03-26 08:28:43 +0800

    ex2

---
+++ 二叉树/101_mirror_tree.cpp
@@ -0,0 +1,18 @@
+/*
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+*/
+
+#include<iostream>
+using namespace std;
+
+bool isMirror(TreeNode *p,TreeNode *q){
+    if(p==NULL&&q==NULL) return  true;
+    if(p!=NULL&&q==NULL||p==NULL&&q!=NULL||p!=NULL&&q!=NULL&&p->val!=q->val) return false;
+    return isMirror(p->right,q->left)&&isMirror(p->left,q->right);
+    return false;
+}
+
+bool isSymmetric(TreeNode *root){
+    if(root==NULL) return true;
+    return isMirror(root->left,root->right);
+}
